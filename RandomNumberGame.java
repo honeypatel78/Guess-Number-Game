1fd52e1ae4c2b8a5d4379f8ef9281c0a0b2b08ce
Honey Patel <honeypatel4264@gmail.com>
2024-07-15 12:33:27 +0530

    RandomNumberGame.java

--- RandomNumberGame.java
+++ RandomNumberGame.java
@@ -1,4 +1,7 @@
- import java.util.Random;
+// Java program to implement 
+// Random number Game
+
+import java.util.Random;
 import java.util.*;
 
 public class RandomNumberGame{
@@ -7,15 +10,25 @@ public class RandomNumberGame{
     {
 
     System.out.println("Welcome to random number game!!");
+
+    //generating a random number in range 0-100
     int mynumber = (int)(Math.random()*100);
+    
     Scanner input = new Scanner(System.in);
     int usernumber = 0;
+
+    //no of available trails
     int count=10;
 
     do{
+    
         System.out.println("Guess the number(0-100)("+count+" chances left)");
+
+        //takes input of a number from user
         usernumber = input.nextInt();
 
+        //if usernumber matches generated number 
+        //then user wins game
         if(usernumber == mynumber)
         {
             System.out.println("WOOHOOO CORRECT NUMBER!\nYOU WON");
@@ -23,21 +36,27 @@ public class RandomNumberGame{
         }
 
 
+        //if usernumber larger than generated number 
         else if( usernumber > mynumber)
         {
-        System.out.println( mynumber + "is larger");
+        System.out.println( "Number " +usernumber+ " is larger");
         }
 
 
+        //if usernumber smaller than generated number 
         else
         {
-            System.out.println( mynumber + "is smaller");
+            System.out.println("Number " +usernumber + " is smaller");
         }
+
+        //decrementing the count
         count--;
 
 
     }
     while(usernumber >= 0 && count>0);
+
+    //if user reach maximum number of trails
     if(count == 0)
     {
         System.out.println("YOU LOST!, maximum limit of incorrect guesses reached.");
@@ -45,6 +64,7 @@ public class RandomNumberGame{
     }
     
 
+}
 }
     
 
